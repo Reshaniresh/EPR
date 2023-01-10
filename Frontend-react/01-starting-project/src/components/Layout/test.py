25493aa8e49981f4e6e55c451fa927f9860d680a
Reshaniresh <reshanireshiba31@gmail.com>
2023-01-10 22:32:26 +0530

    first push-epr

---
+++ Frontend-react/01-starting-project/src/components/Layout/test.py
@@ -0,0 +1,21 @@
+# list1=[1,2,3,4,5,6,7,8,9,10]
+# tuple1=(11,12,13,14,15)
+# print(tuple1)
+# tuple2=list(tuple1)
+# tuple2.append(16)
+# print(tuple2)
+
+# def findprime(prms):
+#     a=[]
+#     for i in range(0,prms):
+#         if i%2==0:
+#             pass
+#         else:
+#             # print("prime number:",i)
+#             a.append(i)
+#     return a
+
+# print(findprime(10))
+
+
+
